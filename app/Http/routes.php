4ec07f143d85d92630179f51d36a748c6a42b3d8
Armen Markossyan <a.a.markossyan@gmail.com>
2014-09-13 18:15:32 +0600

    Redirect root requests to shops.index

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,17 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('/', function() {
+    return Redirect::route('stores.index');
+});
 
 Route::resource('stores', 'StoreController', ['only' => ['index', 'create', 'store', 'destroy']]);
 Route::get('stores/setStoreToken/{id}', ['as' => 'stores.setStoreToken', 'uses' => 'StoreController@setStoreToken']);
+
 Route::resource('stores.hooks', 'HookController', ['only' => ['index', 'create', 'store', 'destroy']]);
 Route::get('auth', ['as' => 'auth', 'uses' => 'AuthController@index']);
